faebd1f4d1cbf96e9640ae18f3e0a0f02530f269
Zhijie Xu <jennyxu000@gmail.com>
2020-10-03 14:20:15 -0700

    finished basic

--- parsgram/FeedViewController.swift
+++ parsgram/FeedViewController.swift
@@ -94,6 +94,7 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -131,6 +132,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         let user = post["author"] as! PFUser
         cell.usernameLabel.text = user.username
         cell.captionLabel.text = post["caption"] as! String
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af_setImage(withURL: url)
         return cell
     }
 }
